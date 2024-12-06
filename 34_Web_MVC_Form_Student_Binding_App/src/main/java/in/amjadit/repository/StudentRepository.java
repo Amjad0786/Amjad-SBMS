e89127989532b70c44b35920e2d24c1b9587300f
Amjad Khan <amjadkhan44734@gmail.com>
2024-12-06 13:00:08 +0530

    reupload

--- 34_Web_MVC_Form_Student_Binding_App/src/main/java/in/amjadit/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package in.amjadit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.amjadit.entity.StudentEntity;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
-
-}
